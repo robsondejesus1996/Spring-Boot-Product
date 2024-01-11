eabf14586433753538b40c82d7d2467dd099bfc2
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-11 16:35:00 -0300

    Modelo para validar requisição

--- back-end/src/main/java/br/com/api/produtos/modole/RespostaModelo.java
+++ back-end/src/main/java/br/com/api/produtos/modole/RespostaModelo.java
@@ -1,5 +1,15 @@
 package br.com.api.produtos.modole;
 
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Component
+@Getter
+@Setter
 public class RespostaModelo {
+
+    private String mensagem;
 
 }
