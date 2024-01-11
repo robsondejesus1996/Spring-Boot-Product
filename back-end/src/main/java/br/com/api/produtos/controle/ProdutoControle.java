78235b91940c980fc1d967ff4d1369e468966bc0
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-11 17:12:30 -0300

    Rota para remover produto.

--- back-end/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ back-end/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -2,13 +2,16 @@ package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modole.ProdutoModelo;
+import br.com.api.produtos.modole.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController
@@ -16,6 +19,12 @@ public class ProdutoControle {
 
     @Autowired
     private ProdutoServico ps;
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo) {
+        return ps.remover(codigo);
+
+    }
 
     @PutMapping("/alterar")
     public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm) {
