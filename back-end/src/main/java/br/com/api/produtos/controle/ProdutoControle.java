19327eccb25f3bd658a1ccdbd46e113d4e347712
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-11 16:40:32 -0300

    Rota de lisar produtos

--- back-end/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ back-end/src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -1,10 +1,23 @@
 package br.com.api.produtos.controle;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.api.produtos.modole.ProdutoModelo;
+import br.com.api.produtos.servico.ProdutoServico;
+
 @RestController
 public class ProdutoControle {
+
+    @Autowired
+    private ProdutoServico ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModelo> listar() {
+
+        return ps.listar();
+    }
 
     @GetMapping("/")
     public String rota() {
