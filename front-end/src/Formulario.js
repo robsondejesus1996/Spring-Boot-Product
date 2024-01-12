34d793f933d11ff1af9189de7ce33fc23b76382b
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-12 08:54:03 -0300

    Estados dos botões

--- front-end/src/Formulario.js
+++ front-end/src/Formulario.js
@@ -1,16 +1,20 @@
-function Formulario(){
-    return (
-        <form>
-            <input type="text" placeholder='Nome' className="form-control"/>
-            <input type="text" placeholder='Marca' className="form-control"/>
+function Formulario({ botao }) {
+  return (
+    <form>
+      <input type="text" placeholder="Nome" className="form-control" />
+      <input type="text" placeholder="Marca" className="form-control" />
 
-            <input type="button" value="cadastrar" className="btn btn-primary"/>
-            <input type="button" value="Alterar" className="btn btn-warning"/>
-            <input type="button" value="Remover" className="btn btn-danger"/>
-            <input type="button" value="Cancelar" className="btn btn-secondary"/>
-        </form>
-    )
+      {botao ? (
+        <input type="button" value="cadastrar" className="btn btn-primary" />
+      ) : (
+        <div>
+          <input type="button" value="Alterar" className="btn btn-warning" />
+          <input type="button" value="Remover" className="btn btn-danger" />
+          <input type="button" value="Cancelar" className="btn btn-secondary" />
+        </div>
+      )}
+    </form>
+  );
 }
-
 
 export default Formulario;
