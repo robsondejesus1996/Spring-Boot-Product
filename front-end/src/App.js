242b707893a37c64ec083dbb9c69f43ec34ec2d6
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-12 09:01:52 -0300

    Teste obtenção dos produtos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,24 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css';
 import Formulario from './Formulario';
 import Tabela from './Tabela';
 
 function App() {
+  //useState
+
   const [btnCadastrar, setBtnCadastrar] = useState(true);
+  const [produtos, setProdutos] = useState([]);
+
+  //useEffect
+  useEffect(()=>{
+    fetch("http://localhost:8080/listar")
+    .then(retorno => retorno.json())
+    .then(retorno_convertido => setProdutos(retorno_convertido));
+  }, []);
 
   return (
     <div className="App">
-
+      <p>{JSON.stringify(produtos)}</p>
       <Formulario botao={btnCadastrar}/>
       <Tabela/>
     </div>
