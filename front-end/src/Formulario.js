eb9af615e19d13405d16dcbc79dcf9292fec7ca3
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-01-12 13:46:33 -0300

    Finalização frond-end

--- front-end/src/Formulario.js
+++ front-end/src/Formulario.js
@@ -1,4 +1,4 @@
-function Formulario({ botao, eventoTeclado, cadastrar,  obj, cancelar}) {
+function Formulario({ botao, eventoTeclado, cadastrar,  obj, cancelar, remover, alterar}) {
   return (
     <form>
       <input type="text" value={obj.nome} onChange={eventoTeclado} name ='nome' placeholder="Nome" className="form-control" />
@@ -8,8 +8,8 @@ function Formulario({ botao, eventoTeclado, cadastrar,  obj, cancelar}) {
         <input type="button" value="cadastrar" onClick={cadastrar} className="btn btn-primary" />
       ) : (
         <div>
-          <input type="button" value="Alterar" className="btn btn-warning" />
-          <input type="button" value="Remover" className="btn btn-danger" />
+          <input type="button" value="Alterar" onClick={alterar} className="btn btn-warning" />
+          <input type="button" value="Remover" onClick={remover} className="btn btn-danger" />
           <input type="button" value="Cancelar" onClick={cancelar} className="btn btn-secondary" />
         </div>
       )}
